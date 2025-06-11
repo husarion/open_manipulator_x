a55d4ccd9a38460d11a3e856fdb53e140d126a72
rafal-gorecki <rafal.gorecki@husarion.com>
2025-06-11 19:20:59 +0200

    Great update Open Manipulation for ROSbot XL Manipulation

--- open_manipulator_x_moveit/launch/setup_assistant.launch.py
+++ open_manipulator_x_moveit/launch/setup_assistant.launch.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_setup_assistant_launch
 
